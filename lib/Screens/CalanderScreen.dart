8eb6a1eed96dec08aa48dd5dee8b1c1de09890af
SnigdhaBarik <snigdhabarik10@gmail.com>
2021-05-22 13:03:14 +0530

    Snig1

---
+++ lib/Screens/CalanderScreen.dart
@@ -0,0 +1,15 @@
+class name extends StatefulWidget {
+  name({Key key}) : super(key: key);
+
+  @override
+  _nameState createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
