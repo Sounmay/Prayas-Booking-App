aed27d19800f1b8b3ee20d9e0f7f616217b2d846
Sounmay <sounmay@yahoo.com>
2021-05-26 13:19:57 +0530

    Sounmay. Search UI added to home

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -34,7 +34,30 @@ class _HomeState extends State<Home> {
             child: Column(
               children: <Widget>[
                 SizedBox(
-                  height: 100.0,
+                  height: 10.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                  height: 70,
+                  child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          prefixIcon: new Icon(Icons.search),
+                          contentPadding: EdgeInsets.all(10),
+                          hintText: 'Search for a service',
+                          fillColor: Colors.white,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                              borderRadius: BorderRadius.circular(6)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black, width: 2.0),
+                              borderRadius: BorderRadius.circular(6))),
+                      onChanged: (val) {}),
+                ),
+                SizedBox(
+                  height: 20.0,
                 ),
                 Container(
                   child: Align(
