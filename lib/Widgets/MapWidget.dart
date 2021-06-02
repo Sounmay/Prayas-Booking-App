3ae9ef71d74503a696de5f855e05955b7f11e498
Sounmay <sounmay@yahoo.com>
2021-06-03 01:13:22 +0530

    Current Location Functionality added

--- lib/Widgets/MapWidget.dart
+++ lib/Widgets/MapWidget.dart
@@ -1,12 +1,26 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatelessWidget {
 
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   GoogleMapController newGoogleMapController;
+  
+  Position currentPosition;
+  var geoLocator=Geolocator();
+  
+  void locatePosition() async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    currentPosition=position;
+    
+    LatLng latLngPosition=LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition = new CameraPosition(target: latLngPosition, zoom: 14);
+    newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -16,15 +30,20 @@ class MapWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height * 0.25,
+        height: MediaQuery.of(context).size.height * 0.3,
         width: MediaQuery.of(context).size.width * 0.9,
       child: GoogleMap(
         mapType: MapType.normal,
         myLocationEnabled: true,
         initialCameraPosition: _kGooglePlex,
+        myLocationButtonEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
         onMapCreated: (GoogleMapController controller){
           _controllerGoogleMap.complete(controller);
           newGoogleMapController=controller;
+
+          locatePosition();
         },
       )
     );
