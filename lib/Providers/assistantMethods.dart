c300e25f80bddbbe55aa17c7186e29505cef6f94
Sounmay <sounmay@yahoo.com>
2021-06-03 21:57:51 +0530

    Sounmay. Address from current location functionality added.

--- lib/Providers/assistantMethods.dart
+++ lib/Providers/assistantMethods.dart
@@ -1,10 +1,11 @@
+import 'package:freelance_booking_app/Providers/configmaps.dart';
 import 'package:freelance_booking_app/Providers/requestAssistant.dart';
 import 'package:geolocator/geolocator.dart';
 
 class AssistantMethods{
   static Future<String> searchCoordinateAddress(Position position) async {
     String placeAddress = "";
-    String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=AIzaSyAlnqnzSpDyzddQeHwJ5cQ56rji_09W2Wc";
+    String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
 
     var response = await RequestAssistant.getRequest(url);
 
