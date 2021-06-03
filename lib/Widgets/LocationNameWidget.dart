21d39ed6c90b04db3923d005bcbdce3b079a00f8
naiyar2000 <naiyarimam.2000@gmail.com>
2021-06-03 17:32:14 +0530

    Naiyar. string function

--- lib/Widgets/LocationNameWidget.dart
+++ lib/Widgets/LocationNameWidget.dart
@@ -6,12 +6,21 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:freelance_booking_app/Providers/assistantMethods.dart';
 
-class LocationNameWidget extends StatelessWidget {
+class LocationNameWidget extends StatefulWidget {
+  @override
+  _LocationNameWidgetState createState() => _LocationNameWidgetState();
+}
+
+class _LocationNameWidgetState extends State<LocationNameWidget> {
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
+
   GoogleMapController newGoogleMapController;
 
   Position currentPosition;
+
   var geoLocator = Geolocator();
+
+  Future location;
 
   Future<String> locationName() async {
     Position position = await Geolocator.getCurrentPosition(
@@ -26,6 +35,15 @@ class LocationNameWidget extends StatelessWidget {
     String address = await AssistantMethods.searchCoordinateAddress(position);
     //variable to be returned
     return address;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      location = locationName();
+      print(location);
+    });
   }
 
   @override
