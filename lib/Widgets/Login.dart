694b0251dc6efa7bfe57caaf402995b9955710af
naiyar2000 <naiyarimam.2000@gmail.com>
2021-06-05 12:21:48 +0530

    Naiyar. text mediaquery in auth pages

--- lib/Widgets/Login.dart
+++ lib/Widgets/Login.dart
@@ -30,48 +30,79 @@ class _LoginState extends State<Login> {
               )
             ]),
           )
-        : Scaffold(
-            body: SingleChildScrollView(
-              child: Container(
-                height: deviceHeight,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                color: Color(0xff5D5FEF),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/splashLogo.png',
-                        height: 120,
-                        width: 120,
-                      ),
-                      SizedBox(
-                        height: deviceHeight * 0.025,
-                      ),
-                      Text("Sign In",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 28)),
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              SizedBox(height: 18.0),
-                              Padding(
-                                  padding: EdgeInsets.only(bottom: 5),
-                                  child: Row(children: [
-                                    Text(
-                                      "Enter Phone Number",
-                                      style: TextStyle(color: Colors.white),
-                                    )
-                                  ])),
-                              TextFormField(
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                      contentPadding: const EdgeInsets.all(0),
-                                      prefixIcon: Icon(Icons.call),
-                                      hintText: 'Phone Number',
+        : MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+            child: Scaffold(
+              body: SingleChildScrollView(
+                child: Container(
+                  height: deviceHeight,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  color: Color(0xff5D5FEF),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/splashLogo.png',
+                          height: 120,
+                          width: 120,
+                        ),
+                        SizedBox(
+                          height: deviceHeight * 0.025,
+                        ),
+                        Text("Sign In",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 28)),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SizedBox(height: 18.0),
+                                Padding(
+                                    padding: EdgeInsets.only(bottom: 5),
+                                    child: Row(children: [
+                                      Text(
+                                        "Enter Phone Number",
+                                        style: TextStyle(color: Colors.white),
+                                      )
+                                    ])),
+                                TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                        contentPadding: const EdgeInsets.all(0),
+                                        prefixIcon: Icon(Icons.call),
+                                        hintText: 'Phone Number',
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Color(0xFF0F2735))),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Color(0xFF0F2735)))),
+                                    validator: (val) => val.isEmpty
+                                        ? 'Enter Your Mobile Number'
+                                        : null,
+                                    onChanged: (val) {
+                                      setState(() => number = val);
+                                    }),
+                                SizedBox(height: 20.0),
+                                Padding(
+                                    padding: EdgeInsets.only(bottom: 5),
+                                    child: Row(children: [
+                                      Text(
+                                        "Enter Password",
+                                        style: TextStyle(color: Colors.white),
+                                      )
+                                    ])),
+                                TextFormField(
+                                    keyboardType: TextInputType.text,
+                                    decoration: InputDecoration(
+                                      contentPadding:
+                                          const EdgeInsets.only(left: 10),
+                                      hintText: 'Password',
                                       fillColor: Colors.white,
                                       filled: true,
                                       focusedBorder: OutlineInputBorder(
@@ -79,122 +110,97 @@ class _LoginState extends State<Login> {
                                               color: Color(0xFF0F2735))),
                                       enabledBorder: OutlineInputBorder(
                                           borderSide: BorderSide(
-                                              color: Color(0xFF0F2735)))),
-                                  validator: (val) => val.isEmpty
-                                      ? 'Enter Your Mobile Number'
-                                      : null,
-                                  onChanged: (val) {
-                                    setState(() => number = val);
-                                  }),
-                              SizedBox(height: 20.0),
-                              Padding(
-                                  padding: EdgeInsets.only(bottom: 5),
-                                  child: Row(children: [
-                                    Text(
-                                      "Enter Password",
-                                      style: TextStyle(color: Colors.white),
-                                    )
-                                  ])),
-                              TextFormField(
-                                  keyboardType: TextInputType.text,
-                                  decoration: InputDecoration(
-                                    contentPadding:
-                                        const EdgeInsets.only(left: 10),
-                                    hintText: 'Password',
-                                    fillColor: Colors.white,
-                                    filled: true,
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color: Color(0xFF0F2735))),
-                                    enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color: Color(0xFF0F2735))),
-                                  ),
-                                  validator: (val) => val.length < 6
-                                      ? 'Enter password of 6+ characters'
-                                      : null,
-                                  obscureText: true,
-                                  onChanged: (val) {
-                                    setState(() => password = val);
-                                  }),
-                              Padding(
-                                  padding: EdgeInsets.only(top: 5),
-                                  child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                          "Forgot Password?",
-                                          style: TextStyle(color: Colors.white),
-                                        )
-                                      ])),
-                              SizedBox(height: 20.0),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.5,
-                                child: TextButton(
-                                  style: TextButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(5),
+                                              color: Color(0xFF0F2735))),
                                     ),
-                                    backgroundColor: Color(0xFF0F2735),
-                                  ),
-                                  child: Text(
-                                    'Cotninue',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 25),
-                                  ),
-                                  onPressed: () async {
-                                    if (_formKey.currentState.validate()) {
-                                      setState(() {
-                                        loading = true;
-                                      });
-                                      String numemail = number + "@gmail.com";
-                                      dynamic result = await _auth
-                                          .signInWithEmailAndPassword(
-                                              numemail, password);
-                                      if (result == null) {
-                                        setState(() {
-                                          error =
-                                              'could not sign in with the credentials';
-                                          loading = false;
-                                        });
-                                      }
-                                    }
-                                  },
-                                ),
-                              ),
-                              SizedBox(height: 10.0),
-                              Text(
-                                error,
-                                style: TextStyle(
-                                    color: Colors.red, fontSize: 14.0),
-                              ),
-                              SizedBox(height: 15.0),
-                              Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(
-                                      'Or, If you are a new user, ',
+                                    validator: (val) => val.length < 6
+                                        ? 'Enter password of 6+ characters'
+                                        : null,
+                                    obscureText: true,
+                                    onChanged: (val) {
+                                      setState(() => password = val);
+                                    }),
+                                Padding(
+                                    padding: EdgeInsets.only(top: 5),
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          Text(
+                                            "Forgot Password?",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          )
+                                        ])),
+                                SizedBox(height: 20.0),
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.5,
+                                  child: TextButton(
+                                    style: TextButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      backgroundColor: Color(0xFF0F2735),
+                                    ),
+                                    child: Text(
+                                      'Cotninue',
                                       style: TextStyle(
-                                          color: Colors.white, fontSize: 15),
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 25),
                                     ),
-                                    InkWell(
-                                      onTap: () {
-                                        widget.toggleView();
-                                      },
-                                      child: Text('Register',
-                                          style: TextStyle(
-                                              color: Colors.red[800],
-                                              decoration:
-                                                  TextDecoration.underline,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18)),
-                                    )
-                                  ])
-                            ]),
-                      ),
-                    ]),
+                                    onPressed: () async {
+                                      if (_formKey.currentState.validate()) {
+                                        setState(() {
+                                          loading = true;
+                                        });
+                                        String numemail = number + "@gmail.com";
+                                        dynamic result = await _auth
+                                            .signInWithEmailAndPassword(
+                                                numemail, password);
+                                        if (result == null) {
+                                          setState(() {
+                                            error =
+                                                'could not sign in with the credentials';
+                                            loading = false;
+                                          });
+                                        }
+                                      }
+                                    },
+                                  ),
+                                ),
+                                SizedBox(height: 10.0),
+                                Text(
+                                  error,
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 14.0),
+                                ),
+                                SizedBox(height: 15.0),
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        'Or, If you are a new user, ',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 15),
+                                      ),
+                                      InkWell(
+                                        onTap: () {
+                                          widget.toggleView();
+                                        },
+                                        child: Text('Register',
+                                            style: TextStyle(
+                                                color: Colors.red[800],
+                                                decoration:
+                                                    TextDecoration.underline,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 18)),
+                                      )
+                                    ])
+                              ]),
+                        ),
+                      ]),
+                ),
               ),
             ),
           );
